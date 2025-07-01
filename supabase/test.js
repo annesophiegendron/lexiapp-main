54fbc8e5d6b08986ceff2dfc38e4eaf0b38deaba
Hawa <104062839+june0809@users.noreply.github.com>
2025-07-01 16:06:22 +0000

    ajout de tags.js et changement pour test.js

--- supabase/test.js
+++ supabase/test.js
@@ -14,11 +14,11 @@ if (loginError) {
 const userId = loginData.user.id
 console.log('Connecté avec user ID :', userId)
 
-/*await createCategory('Test catégorie12', 'hiiii', 'rgba(144, 21, 231, 0.4)', userId);*/
+/*await createCategory('Test catégorie12', 'hiiii', 'rgba(144, 21, 231, 0.4)', userId);
 
 /*import { deleteCategory } from './categories.js';
 
-await deleteCategory('137220fe-eac1-485e-9918-b78ec61072f5');*/
+await deleteCategory('137220fe-eac1-485e-9918-b78ec61072f5');
 
 import { updateCategory } from './categories.js'
 
@@ -28,3 +28,13 @@ await updateCategory(
   'newwww',
   'rgba(113, 100, 255, 0.4)'
 );
+*/
+
+/*import { createTag } from './tags.js';
+await createTag('Essaie numero3', 'rgba(60, 142, 22, 0.67)',userId);
+
+import { updateTag } from './tags.js';
+await updateTag('7971fc6b-aba4-4dca-bf25-b58c4ced7e29','nouvel essaie hihi', 'rgba(24, 22, 142, 0.67)' );*/
+
+import { deleteTag } from './tags.js';
+await deleteTag('e65fe476-a000-479c-bbd5-6047f308ae97');
